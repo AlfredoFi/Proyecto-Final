9e9cfa74003e7fc3fde0d7d282cfa38eaec3827e
AlfredoFi <theproductserial@hotmail.com>
2016-05-15 21:24:59 -0700

    Final Commit

--- client.py
+++ client.py
@@ -1,11 +1,11 @@
 import socket
 
-HOST = 'localhost' 
-PORT = 50007       
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((HOST, PORT))
-voto = raw_input("Ingresa tu Voto: ")
-s.send(voto)
-print s.recv(1024)
-s.close()
+HOST = 'localhost'   #Define el servidor destino
+PORT = 50007       # Define el puerto
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  #Define protocol IPV4 and TCP protocol
+s.connect((HOST, PORT))  # Realiza la conexión al servidor y puerto especificado anteriormente
+voto = raw_input("Ingresa tu Voto: ") #Solicita el nombre del candidato
+s.send(voto) # Envia el voto al servidor
+print s.recv(1024)   #Imprime lo que haya recibido el socket, para recibir es necesaria la conexión del socket
+s.close() # Marca el fin del ciclo del socket
 
--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-import socket
+import socket   
 
-HOST = ''                 
-PORT = 50007             
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((HOST, PORT))
+HOST = ''                #Define all possible hosts
+PORT = 50007             #Working port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Define protocol IPv4 and TCP protocol
+s.bind((HOST, PORT))   #Bind the socket to address
 s.listen(5)
 while True:
-    conn,addr = s.accept()    
+    conn,addr = s.accept()  #Accept a connection. The socket must be bound to an address and listening for connections.  
     print 'Conexion realizada desde: ',addr
-    data = conn.recv(1024) 
+    data = conn.recv(1024)  #Enable a server to accept connections. If backlog is specified, it must be at least 0
     print 'El voto fue para: ',data
-    conn.send('Gracias por utilizar el servicio')  
-    conn.close()               
+    conn.send('Gracias por utilizar el servicio')  #Send data to the socket. The socket must be connected to a remote socket. 
+    conn.close()               #Mark the socket closed.
